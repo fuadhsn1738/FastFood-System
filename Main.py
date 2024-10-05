22a0128b8d0b157809da0f819841e789847fbd58
Fuad Hassan <fuad.hassan02@northsouth.edu>
2024-10-05 19:04:15 +0600

    Splashscreen display
    Created the code to read from the Splash.txt file and display it. Also fixed Splash.txt misalignment.

---
+++ Main.py
@@ -0,0 +1,9 @@
+import os, time
+
+os.system("cls")
+splash = open(r"E:\Coding\Python\Restaurant\FastFood-System\Splash.txt", "r")
+splash_read = splash.read()
+print(splash_read)
+splash.close()
+time.sleep(2)
+os.system("cls")
